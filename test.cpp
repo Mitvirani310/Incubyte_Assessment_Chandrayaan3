86a4f4ecd6f140134b7b013b867af869acc283eb
Mitvirani310 <86394017+Mitvirani310@users.noreply.github.com>
2023-08-04 13:50:48 +0530

    Testing_direction_from_south_fail_Testcase

--- test.cpp
+++ test.cpp
@@ -48,3 +48,52 @@ TEST(TestDirections, TestDirectionsFromNorthToRight) {
 	EXPECT_TRUE(true);
 
 }
+
+
+TEST(TestDirections, TestDirectionsFromSouthToDown) {
+	char dir = 'S';
+	vector<int>position = { 0,0,0 };
+	Chandrayan* chandrayan = new Chandrayan(dir, position);
+
+	chandrayan->turn_down();
+	char newdir = chandrayan->getSpacecraftDirection();
+	EXPECT_EQ('D', newdir);
+	EXPECT_TRUE(true);
+}
+
+
+TEST(TestDirections, TestDirectionsFromSouthToUp) {
+	char dir = 'S';
+	vector<int>position = { 0,0,0 };
+	Chandrayan* chandrayan = new Chandrayan(dir, position);
+
+	chandrayan->turn_up();
+	char newdir = chandrayan->getSpacecraftDirection();
+	EXPECT_EQ('U', newdir);
+	EXPECT_TRUE(true);
+
+}
+
+TEST(TestDirections, TestDirectionsFromSouthToLeft) {
+	char dir = 'S';
+	vector<int>position = { 0,0,0 };
+	Chandrayan* chandrayan = new Chandrayan(dir, position);
+
+	chandrayan->turn_left();
+	char newdir = chandrayan->getSpacecraftDirection();
+	EXPECT_EQ('E', newdir);
+	EXPECT_TRUE(true);
+
+}
+
+TEST(TestDirections, TestDirectionsFromSouthToRight) {
+	char dir = 'S';
+	vector<int>position = { 0,0,0 };
+	Chandrayan* chandrayan = new Chandrayan(dir, position);
+
+	chandrayan->turn_right();
+	char newdir = chandrayan->getSpacecraftDirection();
+	EXPECT_EQ('W', newdir);
+	EXPECT_TRUE(true);
+
+}
